72897f6cf36e6e150b1ca0646d9da1385e92e58c
Alexy Grabov <alexy.grabov@cyberark.com>
2022-11-23 10:09:38 +0200

    resolve dependency tree concurrently

--- flattener.py
+++ flattener.py
@@ -1,29 +1,39 @@
+import concurrent.futures
 import subprocess
 from pathlib import Path
 from typing import Set, List
 
 
-def generate_deps_set(requirements_txt_file: Path = None, package_name: str = None) -> Set[str]:
+def generate_deps_set(requirements_txt_file: Path = None, package_names: List[str] = None) -> Set[str]:
     """
     Generates a fully resolved, flat requirements list from a requirements.txt file or a package name
-    :param package_name: name of a single python package you want to resolve dependencies for
+
+    :param package_names: names of python packages you want to resolve dependencies for
     :param requirements_txt_file: a generated or a manually created requirements.txt file.
     :return: A set of recursively resolved dependencies.
     """
-    if not requirements_txt_file and not package_name:
+    if not requirements_txt_file and not package_names:
         raise ValueError("must supply either file Path or package name.")
 
     if requirements_txt_file:
         with open(requirements_txt_file.as_posix(), 'r') as req_file:
-            file_lines = req_file.readlines()
+            requirements_list = req_file.readlines()
     else:
-        file_lines = [package_name]
+        requirements_list = package_names
 
     dependencies_set: Set[str] = set()
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+    futures = []
 
-    for line in file_lines:
+    for line in requirements_list:
         package_name: str = line.split('==')[0]
-        _add_dependencies_recursively(package_name, dependencies_set)
+        futures.append(executor.submit(_add_dependencies_recursively, package_name=package_name, dependencies_set=dependencies_set))
+        #_add_dependencies_recursively(package_name, dependencies_set)
+
+    concurrent.futures.wait(futures)
+    executor.shutdown()
+    # remove AWS natively-provided dependencies
+    dependencies_set.difference_update({'aws-cdk', 'boto3', 'docker, botocore'})
 
     return dependencies_set
 
@@ -36,9 +46,6 @@ def _add_dependencies_recursively(package_name: str, dependencies_set: Set[str])
     :param dependencies_set: string set of dependencies. must be empty on first call.
     """
     print(f'current dependencies set: {str(dependencies_set)}')
-    # remove if not used to Package Lambdas for AWS:
-    if not package_name or any(native_package in package_name for native_package in ['aws-cdk', 'boto3', 'docker']):
-        return
     rv = subprocess.run(['python', '-m', 'pip', 'show', package_name], capture_output=True)
     print(f'adding package {package_name}')
     dependencies_set.add(package_name)
@@ -52,5 +59,3 @@ def _add_dependencies_recursively(package_name: str, dependencies_set: Set[str])
                     continue
                 _add_dependencies_recursively(requirement, dependencies_set)
             break
-
-
