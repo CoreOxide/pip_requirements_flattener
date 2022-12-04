fde8f207ed9c845cfb3a9b3f110b57e138027528
Alexy Grabov <58911092+CoreOxide@users.noreply.github.com>
2022-12-04 14:59:58 +0200

    minor style fixes. (#4)
    Co-authored-by: Alexy Grabov <alexy.grabov@cyberark.com>

--- flattener.py
+++ flattener.py
@@ -7,27 +7,27 @@ from typing import Set, List
 def generate_deps_set(requirements_txt_file: Path = None, package_names: List[str] = None) -> Set[str]:
     """
     Generates a fully resolved, flat requirements list from a requirements.txt file or a package name
-
     :param package_names: names of python packages you want to resolve dependencies for
     :param requirements_txt_file: a generated or a manually created requirements.txt file.
     :return: A set of recursively resolved dependencies.
     """
     if requirements_txt_file is None and package_names is None:
-        raise ValueError("must supply either file Path or package name.")
+        raise ValueError('must supply either file Path or package name.')
 
     if requirements_txt_file:
-        with open(requirements_txt_file.as_posix(), 'r') as req_file:
+        with open(requirements_txt_file.as_posix(), 'r', encoding='utf-8') as req_file:
             requirements_list = req_file.readlines()
     else:
         requirements_list = package_names
 
     dependencies_set: Set[str] = set()
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
-    futures = []
 
-    for line in requirements_list:
-        package_name: str = line.split('==')[0]
-        futures.append(executor.submit(_add_dependencies_recursively, package_name=package_name, dependencies_set=dependencies_set))
+    package_names = [line.split('==', maxsplit=1)[0] for line in requirements_list]
+    futures = [
+        executor.submit(_add_dependencies_recursively, package_name=package_name, dependencies_set=dependencies_set)
+        for package_name in package_names
+    ]
 
     concurrent.futures.wait(futures)
     executor.shutdown()
@@ -40,18 +40,17 @@ def generate_deps_set(requirements_txt_file: Path = None, package_names: List[st
 def _add_dependencies_recursively(package_name: str, dependencies_set: Set[str]) -> None:
     """
     Updates passed dependencies set with all dependencies from the packages' dependency tree, recursively.
-
     :param package_name: packages you want to resolve dependencies for.
     :param dependencies_set: string set of dependencies. must be empty on first call.
     """
     print(f'current dependencies set: {str(dependencies_set)}')
-    rv = subprocess.run(['python', '-m', 'pip', 'show', package_name], capture_output=True, check=True)
+    return_value = subprocess.run(['python', '-m', 'pip', 'show', package_name], capture_output=True, check=True)
     print(f'adding package {package_name}')
     dependencies_set.add(package_name)
-    text_output: str = rv.stdout.decode()
+    text_output: str = return_value.stdout.decode()
     for item in text_output.splitlines():
         if 'Requires' in item:  # if this package has sub-dependencies
-            requirements_list: List[str] = item.split(":", maxsplit=1)[1].replace(" ", "").split(",")
+            requirements_list: List[str] = item.split(':', maxsplit=1)[1].replace(' ', '').split(',')
             for requirement in requirements_list:
                 if not requirement or requirement in dependencies_set:
                     print(f'package already added - {requirement}, skipping.')
